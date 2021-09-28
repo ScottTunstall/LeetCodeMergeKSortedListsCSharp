937a99e72bd9aa8739247c6d96d1f33140c84492
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-09-28 13:27:14 +0100

    Well this was easy. Got this algorithm 'right' (as in, working) first time. Memory Usage: 29.7 MB, less than 75.71% of C# online submissions for Merge k Sorted Lists.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeMergeKSortedListsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMergeKSortedListsCSharp
+{
+
+    public class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0)
+                return null;
+
+            ListNode head = UnqueueNodeWithMinValue(lists);
+            if (head == null)
+                return null;
+            
+            ListNode tail = head;
+
+            ListNode nextNode;
+            while ((nextNode = UnqueueNodeWithMinValue(lists)) != null)
+            {
+                tail.next = nextNode;
+                tail = tail.next;
+            }
+
+            return head;
+        }
+
+        private ListNode UnqueueNodeWithMinValue(ListNode[] lists)
+        {
+            int minVal = int.MaxValue;
+            ListNode nodeWithMinValue = null;
+            ListNode replacementHeadForNodeWithMinValue = null;
+            int indexOfListWithMinVal = 0;
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                if (lists[i]!=null && lists[i].val < minVal)
+                {
+                    nodeWithMinValue = lists[i];
+                    replacementHeadForNodeWithMinValue = nodeWithMinValue.next;
+                    indexOfListWithMinVal = i;
+                    minVal = lists[i].val;
+                }
+            }
+
+            // Replace head of list[indexOfListWithMinVal
+            lists[indexOfListWithMinVal] = replacementHeadForNodeWithMinValue;
+
+            return nodeWithMinValue;
+        }
+    }
+}
